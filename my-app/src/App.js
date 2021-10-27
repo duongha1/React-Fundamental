ee478d4d1e5adef7dc71e32ae89f5a30e2fd12b6
Duong Ha <hm.duong1995@gmail.com>
2021-10-27 12:29:00 +0300

    6.1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,31 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-	const [calculation, setCalculation] = useState({ num1: 0, num2: 0 });
-	const [result, setResult] = useState(0);
-	const [sum, setSum] = useState(0);
+	const [users, setUsers] = useState([]);
 
-	const formSubmitted = (event) => {
-		event.preventDefault();
-		if (sum === 1) {
-			setResult(calculation.num1 + calculation.num2);
-		} else {
-			setResult(calculation.num1 - calculation.num2);
-		}
-	};
-
-	const inputChanged = (event) => {
-		setCalculation({
-			...calculation,
-			[event.target.name]: Number(event.target.value),
-		});
-	};
+	useEffect(() => {
+		fetch("https://reqres.in/api/users")
+			.then((response) => response.json())
+			.then((resData) => setUsers(resData.data));
+	}, []);
 
 	return (
 		<div className="App">
-			<p>Result = {result}</p>
-			<br />
-			<form onSubmit={formSubmitted}>
-				<input
-					placeholder="number 1"
-					name="num1"
-					value={calculation.num1}
-					onChange={inputChanged}
-				/>
-				<input
-					placeholder="number 2"
-					name="num2"
-					value={calculation.num2}
-					onChange={inputChanged}
-				/>
-				<input type="submit" value="+" onClick={() => setSum(1)} />
-				<input type="submit" value="-" onClick={() => setSum(0)} />
-			</form>
+			<table>
+				<tbody>
+					{users.map((user, index) => (
+						<tr key={index}>
+							<td>{user.first_name}</td>
+							<td>{user.last_name}</td>
+							<td>{user.email}</td>
+							<td>
+								<img src={user.avatar} alt={user.last_name} />
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 }
